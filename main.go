67595164b9b2d54357836ec26756c12d614490df
Wattanapong Thongsoi <105178911+thongsoi@users.noreply.github.com>
2025-05-06 16:20:21 +0700

    procedural paradigm

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
+import "fmt"
+
+func add(a, b int) int {
+	return a + b
+}
 
 func main() {
-	db := InitDB()
-	defer db.Close()
-
-	service := NewPGUserService(db)
-	handler := NewUserHandler(service)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.Index)
-	mux.HandleFunc("/register", handler.CreateUser)
-
-	log.Println("Server running on http://localhost:8000")
-	http.ListenAndServe(":8000", mux)
+	result := add(3, 4)
+	fmt.Println(result)
 }
